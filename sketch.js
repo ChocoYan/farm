730faca9da3017dad4d3cf3072745cd44f8ba1fa
ChocoYan <102587208+ChocoYan@users.noreply.github.com>
2023-05-20 15:27:38 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var bgimg,bg;
+var cheeseimg,breadimg,paproniimg,basilimg
+
+function preload(){
+    bgimg=loadImage("assets/splash.gif")
+
+
+}
+
+
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+
+
+}
+
+function draw(){
+    background(bgimg)
+
+}
